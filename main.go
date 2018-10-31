3b43ab935be8118953bda039ca0d5ea4a980526b
Bruce.Smith <Bruce.Smith.IT@gmail.com>
2018-10-30 22:57:44 -0400

    expanded memory and characteristics

--- main.go
+++ main.go
@@ -14,14 +14,57 @@ func main() {
 		fmt.Printf("Unable to get dmidecode information. Error: %v\n", err)
 	}
 
-	replacer := strings.NewReplacer("-", "_", " ", "_")
+	replacer := strings.NewReplacer(":", "_", "-", "_", " ", "_")
 	x := make(map[string][]string)
 
+	// Iterate on Top of all the DMI Data
 	for _, records := range dmi.Data {
+		// Iterate on Each Record Type
 		for _, record := range records {
-			//fmt.Println(record)
+			// Expand And Model Data Most Appropriate for each Type
+			//fmt.Println(record["DMIName"])
+
+			// Clean Empty Values
 			for k, v := range record {
-				x[replacer.Replace(strings.ToLower(record["DMIName"]))] = append(x[replacer.Replace(strings.ToLower(record["DMIName"]))], fmt.Sprintf("%s=\"%s\"", replacer.Replace(strings.ToLower(k)), strings.Replace(v, "\"", "\\\"", -1)))
+				if v == " " {
+					delete(record, k)
+				}
+
+			}
+
+			// Memory Device
+			if record["DMIName"] == "Memory Device" {
+				keyName := replacer.Replace(strings.ToLower(record["DMIName"])) + "_" + replacer.Replace(strings.ToLower(record["Locator"])) + "_" + replacer.Replace(strings.ToLower(record["Bank Locator"]))
+				for k, v := range record {
+					x[keyName] = append(x[keyName], fmt.Sprintf("%s=\"%s\"", replacer.Replace(strings.ToLower(k)), strings.Replace(v, "\"", "\\\"", -1)))
+				}
+
+			}
+
+			// Bios Information
+			if record["DMIName"] == "BIOS Information" {
+				keyName := replacer.Replace(strings.ToLower(record["DMIName"]))
+				// Get Characteristics Array
+				formatedCharacteristics := strings.ToLower(record["Characteristics"])
+				characteristics := strings.Split(formatedCharacteristics, "\t\t")
+				// Flatten Chars
+				for _, v := range characteristics {
+					record["characteristics"+"_"+fmt.Sprintf(replacer.Replace((v)))] = "true"
+
+				}
+				delete(record, "Characteristics")
+
+				for k, v := range record {
+					x[keyName] = append(x[keyName], fmt.Sprintf("%s=\"%s\"", replacer.Replace(strings.ToLower(k)), strings.Replace(v, "\"", "\\\"", -1)))
+				}
+
+			}
+			// Process the rest without special formatting
+			if record["DMIName"] != "BIOS Information" || record["DMIName"] != "Memory Device" {
+				for k, v := range record {
+
+					x[replacer.Replace(strings.ToLower(record["DMIName"]))] = append(x[replacer.Replace(strings.ToLower(record["DMIName"]))], fmt.Sprintf("%s=\"%s\"", replacer.Replace(strings.ToLower(k)), strings.Replace(v, "\"", "\\\"", -1)))
+				}
 			}
 		}
 	}
